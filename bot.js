e922006079ce56b63cbd0a8b646f177fbf62ff9e
LoadedGuns <jsweetsir18@icloud.com>
2023-12-12 13:35:02 -0500

    Startup Files

---
+++ bot.js
@@ -0,0 +1,3 @@
+// ID: 1131936033546698887
+// TOKEN: MTEzMTkzNjAzMzU0NjY5ODg4Nw.GFUkRj.ZDu1jXwWLlrM78opMXW8KzkEL5JK_htnh-dMAs
+// Invite Link: https://discord.com/oauth2/authorize?client_id=1131936033546698887&scope=bot&permissions=1
